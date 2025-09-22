2cff1e58a7e4d8a6313e6be6d5034ad092660ccc
DeadChuck06 <dishawalnekar160@gmail.com>
2025-09-22 20:39:52 +0530

    Update server for Render deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '..')));
 
 
 // 👇 MongoDB Atlas connection
-mongoose.connect('mongodb+srv://dishawalnekar160_db_user:FRmagMGfeXj08lLV@cliniccluster.ciyxmde.mongodb.net/?retryWrites=true&w=majority&appName=ClinicCluster', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -88,6 +88,8 @@ app.delete('/appointments/:id', async (req, res) => {
 
 
 // Start server
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
